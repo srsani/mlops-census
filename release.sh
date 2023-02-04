19f0e26097d2cd9ea787c8cf137f92ad554ab0cc
Sohrab Redjai Sani <srs@Sohrabs-MBP.home>
2023-02-04 20:00:33 +0000

    updated action

--- release.sh
+++ release.sh
@@ -5,11 +5,8 @@ echo $branch_name
 echo build and push Docker image to ECR
 sh docker_ecr.sh latest
 
+echo Installing compose-cli
 curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
 
-docker context create ecs ecs-context --from-env
-docker --context ecs-context compose up
-
-# sudo docker context create ecs srs-fastapi
-# docker context use srs-fastapi
-# docker compose up
+# docker context create ecs ecs-context --from-env
+# docker --context ecs-context compose up
