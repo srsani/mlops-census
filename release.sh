9d1a1f208511df8239eedd8391ea4a84a4130e10
Sohrab Redjai Sani <srs@Sohrabs-MBP.home>
2023-02-04 19:22:22 +0000

    action ecr fix

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ echo $branch_name
 echo build and push Docker image to ECR
 sh docker_ecr.sh latest
 
-docker context create ecs srs-fastapi
+curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
+
+sudo docker context create ecs srs-fastapi
 docker context use srs-fastapi
 docker compose up
